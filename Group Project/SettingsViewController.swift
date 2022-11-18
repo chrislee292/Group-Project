58cb3d689341baaf435ebc3c19d8389c58af7113
chrislee292 <98063243+chrislee292@users.noreply.github.com>
2022-11-17 23:54:47 -0600

    Add Settings View Controller - Jayashree
    Add Settings View Controller and Buttons

---
+++ Group Project/SettingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  SettingsViewController.swift
+//  Group Project
+//
+//  Created by Jayashree Ganesan on 11/17/22.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBAction func notifToggle(_ sender: Any) {
+    }
+    
+    @IBAction func darkModeToggle(_ sender: Any) {
+    }
+
+    @IBAction func segmentControlFont(_ sender: Any) {
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    @IBAction func logOutButton(_ sender: Any) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
